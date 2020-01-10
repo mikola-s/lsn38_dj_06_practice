d3c7ea92c76f915da0f8f782a72782f9fd120743
mikola-s <suharev.nv@gmail.com>
2020-01-10 09:02:24 +0200

    lsn38 zoo/models.py add Animal & make/migration

--- zoo/models.py
+++ zoo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class AnimalType(models.Model):
@@ -13,3 +14,14 @@ class AnimalGender(models.Model):
 
     def __str__(self):
         return self.gender
+
+
+class Animal(models.Model):
+    animal_type = models.ForeignKey(to=AnimalType, on_delete=models.SET_DEFAULT, default='this')
+    gender = models.ForeignKey(to=AnimalGender, on_delete=models.SET_DEFAULT, default='it')
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
+    name = models.CharField(max_length=256, default='organism')
+    incoming_time = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+    def __str__(self):
+        return f"{self.name} ({self.animal_type})"
