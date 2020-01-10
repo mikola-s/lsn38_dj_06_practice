3c871320c2a5f5e1f06c5910d5bd2f7d3c1a2136
mikola-s <suharev.nv@gmail.com>
2020-01-10 08:45:50 +0200

    lsn38 zoo/models.py add AnimalType & make/migration

--- zoo/models.py
+++ zoo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class AnimalType(models.Model):
+    animal_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.animal_type
+
