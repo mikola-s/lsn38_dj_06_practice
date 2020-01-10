ffd5263887f9bc049d1cbe8eee9cf9fdce4118a1
mikola-s <suharev.nv@gmail.com>
2020-01-10 13:55:04 +0200

    lsn38 zoo/models.py add Visitor & make/migration

--- zoo/models.py
+++ zoo/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class AnimalType(models.Model):
@@ -34,6 +37,20 @@ class AgeRange(models.Model):
 
     def __str__(self):
         return f"{self.name} (from {self.age_from} to {self.age_to})"
+
+
+class Visitor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Visitor.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 
 class Ticket(models.Model):
