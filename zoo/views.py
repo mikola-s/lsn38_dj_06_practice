99534f2a2ebe84f9a577a7933a274a5ba7297a01
mikola-s <suharev.nv@gmail.com>
2020-01-12 21:41:32 +0200

    lsn38 zoo/models.py VisitingAnimals edit __str__

--- zoo/views.py
+++ zoo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.utils import timezone
 from . import models
 
 
@@ -31,3 +32,16 @@ class AnimalList(ListView):
 
 class AnimalDetails(DetailView):
     model = models.Animal
+    context_object_name = "animal"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.order_by('visitinganimals__visit_time')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        time_delta = timezone.now() - timezone.timedelta(minutes=5)
+        animal = self.model.objects.filter(pk=self.kwargs['pk'])
+        visit_in_five_min = animal.filter(visitinganimals__visit_time__gte=time_delta).count()
+        context['visit_in_five_min'] = visit_in_five_min
+        return context
